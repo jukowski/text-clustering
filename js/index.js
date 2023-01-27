3d7aa74977498bfe4ad3bbb57e2d8c2dba08dc86
Constantin Jukowski <constantin.jukowski@gmail.com>
2023-01-27 01:49:24 +0100

    simplifying

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-import("../pkg/index.js").catch(console.error).then(m => {
-    const Index = m.Index;
-    const i = Index.new(42, 3);
+import("../pkg/index.js").catch(console.error).then(module => {
+    const Index = module.Index;
+    console.log(Index);
+    const i = Index.new(42, 3, 0.5);
     window.i = i;
     i.insert(0, "Hello");
     i.insert(1, "Hello world");
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use js_sys::Array;
 use wasm_bindgen::prelude::*;
+use fnv::FnvBuildHasher;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,48 +21,26 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub struct Index {
     num_bands: usize,
     band_width: usize,
-    hasher : Arc<Mutex<MinHashIndex<u32, usize>>>
+    min_hasher: MinHasher32<FnvBuildHasher>,
+    hasher : MinHashIndex<u32, usize>
 }
 
 #[wasm_bindgen]
 impl Index {
-    pub fn new(num_bands: usize, band_width: usize) -> Index {
+    pub fn new(num_bands: usize, band_width: usize, threshold: f64) -> Index {
         Index {
             num_bands, band_width,
-            hasher : Arc::new(Mutex::new(MinHashIndex::new(num_bands, band_width, 0.5)))
+            min_hasher: MinHasher32::new(num_bands * band_width),
+            hasher : MinHashIndex::new(num_bands, band_width, threshold)
         }
     }
 
     pub fn size(&self) -> usize {
-        return self.hasher.lock().unwrap().size();
+        return self.hasher.size();
     }
 
     pub fn insert(&mut self, id: usize, doc : String) {
-        let minhasher = MinHasher32::new(self.num_bands * self.band_width);
-        let sig = minhasher.create_signature(whitespace_split(&doc.to_lowercase()));
-        let mut idx = self.hasher.lock().unwrap();
-        idx.insert(id, sig);
+        let sig = self.min_hasher.create_signature(whitespace_split(&doc.to_lowercase()));
+        self.hasher.insert(id, sig);
     }
 }
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-/*
-#[wasm_bindgen]
-pub fn init() -> Index {
-    Index::new(42, 3)
-}
-
-#[wasm_bindgen]
-pub fn add(index : &mut Index, corpus: Array) {
-    let (num_bands, band_width) = (42, 3);
-
-    for (i, doc) in corpus.iter().enumerate() {
-        let d : String = doc.as_string().unwrap();
-        index.insert(i, d);
-    }
-}
-*/
